a6b398898aceee04b266df200600be70f8a06deb
Xianpeng Zhao <zhaoxianpeng@aliyun.com>
2016-10-22 23:25:07 +0800

    trainlist can get, and train code to no done. the query work block by the passcode verification, can download the picture of pass code, but cannot post the pass code to server, so that the pass code invalid always

--- 12306.py
+++ 12306.py
@@ -5,54 +5,221 @@
 import requests
 import re
 import json
+import sys
+import random
+import urllib
 
 # disable warning
 from requests.packages.urllib3.exceptions import InsecureRequestWarning,InsecurePlatformWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 requests.packages.urllib3.disable_warnings(InsecurePlatformWarning)
 
-class train(object):
-    __init__(self,number='',from_station='',to_station='',
-             date='', begin='',end='',pass_station=''):
-        self.number=number;
-        self.from_station=from_station;
-        self.to_station=to_station;
-        self.date=date;
-        self.begin=begin;
-        self.end=end;
-        self.pass_station=pass_station;
+# Set default encoding to utf-8
+
+
+common_headers = {
+    "Host": "kyfw.12306.cn",
+    "Connection": "keep-alive",
+    "Cache-Control": "no-cache",
+    "Accept": "*/*",
+    "x-Requested-With": "XMLHttpRequest",
+    "If-Modified-Since": "0",
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
+    "Accept-Encoding": "gzip, deflate, sdch, br",
+    "Accept-Language": "zh-CN,zh;q=0.8"
+
+}
+
+specific_headers={
+    'queryByTrainNo':{
+        'Referer': 'https://kyfw.12306.cn/otn/queryTrainInfo/init',
+        'method':'GET'
+    },
+    'query_trainlist':{
+        'Referer': 'https://kyfw.12306.cn/otn/queryTrainInfo/init',
+        'method':'GET'
+    },
+    'get_pass_code':{
+        'Referer': 'https://kyfw.12306.cn/otn/queryTrainInfo/init',
+        'method':'GET'
+    },
+    'checkRandCodeAnsyn':{
+        'Referer': 'https://kyfw.12306.cn/otn/queryTrainInfo/init',
+        'method':'POST',
+        'Cache-Control': 'no-cache',
+        'x-requested-with': 'XMLHttpRequest',
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'Origin': 'https://kyfw.12306.cn'
         
+    }    
+}
+
+urls={
+    'queryByTrainNo':'https://kyfw.12306.cn/otn/queryTrainInfo/query?',
+    'query_trainlist':'https://kyfw.12306.cn/otn/resources/js/query/train_list.js?scriptVersion=1.0',
+    'get_pass_code':'https://kyfw.12306.cn/otn/passcodeNew/getPassCodeNew?',
+    'checkRandCodeAnsyn':'https://kyfw.12306.cn/otn/passcodeNew/checkRandCodeAnsyn'
+}
+
+def session_header_update(old_header, new_headers):
+    keys = [
+        'Referer',
+        'Cache-Control',
+        'x-requested-with',
+        'Content-Type',
+        'Origin'
+    ]
+    for key in keys:
+        if key in new_headers:
+            old_header[key]=new_headers[key]; 
+
+# 列车
+class train(object):
+    def __init__(self, train_no='', train_code='', date=''):
+        self.train_no=train_no;
+        self.train_code=train_code;
+        self.date=date;
+        if train_no == '' and train_code != '':
+            self.train_no = self.train_code2no(train_code);
+            
+    def __search_train_no_by_code(self, train_code, train_list):
+        key1 = sorted(train_list.keys(),reverse=True)[0];
+        print(key1)
+        for key2 in train_list[key1].keys():
+            for one_train in train_list[key1][key2]:
+                train_code_in_list = one_train['station_train_code'].upper();
+                if train_code_in_list.find(train_code.upper()+'(') != -1:
+                    train_no = train_code_in_list = one_train['train_no']
+                    return train_no;
+                    
+    def train_code2no(self, train_code=''):
+        if train_code == '':
+            train_code = self.train_code;
+        if train_code == '':
+            print("Please input correct train code")
+            return None;
+        
+        s = requests.Session();
+        my_header = common_headers;
+        session_header_update(my_header, specific_headers['query_trainlist']);
+        req=requests.Request(specific_headers['query_trainlist']['method'], urls['query_trainlist'], headers=my_header);
+        prepped=s.prepare_request(req);
+        
+        i = 0
+        while i < 10:
+            resp=s.send(prepped, verify=False);
+            # resp.content is var train_list = {}
+            #print(resp.content)
+            str=resp.text;
+            if str.find(u'train_list =') != -1:
+                print(u'查询成功');
+                str_list = str.split('=')
+                train_list=json.loads(str_list[1]);
+
+                for key in train_list:
+                    #print(key)
+                    pass
+                break;
+            else:
+                i = i + 1;
+                print(u'查询失败,重试第%d次' %i);
+        if i == 11:
+            print(u'查询失败');
+            return None;
+        
+        # 查询成功，开始解析数据， train_list包含所有的信息
+        train_no = self.__search_train_no_by_code(train_code, train_list);
+        print(u'%s对应的NO是%s' %(train_code, train_no));
+        return train_no;
+        
+    def __get_pass_code(self, module='other'):
+        d = {'other':{'rand':'sjrand'}}
+        if not module in d:
+            print(u'无效的 module: %s' % (module))
+            return None;
+        
+        s = requests.Session();
+        my_header = common_headers;
+        session_header_update(my_header, specific_headers['get_pass_code']);
+        
+        url = '%smodule=%s&rand=%s&' % (urls['get_pass_code'], module, d[module]['rand']);
+        req=requests.Request(specific_headers['get_pass_code']['method'], url, headers=my_header);
+        prepped=s.prepare_request(req);
+        resp=s.send(prepped, verify=False);
+        cookies=requests.utils.dict_from_cookiejar(s.cookies);
+        print(cookies)
+
+        with open('captcha.bmp', 'wb') as fd:
+            for chunk in resp.iter_content():
+                fd.write(chunk)    
+        print(u'请输入4位图片验证码(回车刷新验证码):')
+        passcode = input() 
+        
+        my_header = common_headers;
+        session_header_update(my_header, specific_headers['checkRandCodeAnsyn']);
+        payload={'rand':d[module]['rand'], 'randcode':passcode};
+        url = '%s' % (urls['checkRandCodeAnsyn']);
+        req=requests.Request(specific_headers['checkRandCodeAnsyn']['method'], url, cookies=cookies, headers=my_header, data=payload);
+        #prepped=s2.prepare_request(req); 
+        #resp=s2.send(prepped, verify=False);
+        resp=s.post(url, data=json.dumps(payload), headers=my_header, cookies=cookies, verify=False);
+        
+        
+        print(resp.url)
+        str=resp.content; 
+        print(str);            
+        return passcode;
+    
+    def query_by_train_no(self,train_no='',date=''):
+        if train_no == '':
+            train_no = self.train_no;
+        
+        if train_no == '':
+            print("Please input correct train_no");
+            return None;
+        
+        if date == '':
+            date = self.date;
+        
+        if date == '':
+            print(u"请输入日期");
+            return None;
+        
+        pass_code = self.__get_pass_code(module='other');
+        
+        # we get correct train_no now, start query
+        s = requests.Session();
+        my_header = common_headers;
+        session_header_update(my_header, specific_headers['queryByTrainNo']);
+        url = '%sleftTicketDTO.train_no=%s&leftTicketDTO.train_date=%s&rand_code=%s' %(urls['queryByTrainNo'],train_no,date,pass_code);
+        req=requests.Request(specific_headers['queryByTrainNo']['method'], url, headers=my_header);
+        prepped=s.prepare_request(req);
+        resp=s.send(prepped, verify=False);
+        
+        # resp.content is var train_list = {}
+        #print(resp.content)
+        str=resp.text; 
+        print(str);
+
+#乘客
+class passenger(object):
+    pass
+
+#订单
+class order(object):
+    pass
+
+def main():
+    one_train=train(train_code='k1181', date='2016-10-28');
+    one_train.query_by_train_no();
     
 
-
-
-
-
+    
+    
 if __name__ == '__main__':
+    main();
     
-    # 获取网页数据
-    header = {
-        "Host": "kyfw.12306.cn",
-        "Connection": "keep-alive",
-        "Cache-Control": "no-cache",
-        "Accept": "*/*",
-        "X-Requested-With": "XMLHttpRequest",
-        "If-Modified-Since": "0",
-        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
-        "Referer": "https://kyfw.12306.cn/otn/leftTicket/init",
-        "Accept-Encoding": "gzip, deflate, sdch, br",
-        "Accept-Language": "en-us;q=0.5,en;q=0.3"
-    }
-    url = "https://kyfw.12306.cn/otn/leftTicket/queryC" + "?" + "leftTicketDTO.train_date=" + "2016-10-22" + "&leftTicketDTO.from_station=" + "ZBK" + "&leftTicketDTO.to_station=" + "HGH" + "&purpose_codes=" + "ADULT"
-    r = requests.get(url, headers=header, verify=False)
-    train = r.json();
-    
-    #分析抓取的数据
-    #print(type(train))
-    #print json.dumps(train, indent=4)
-    print("所有车次")
-    for train_data in train['data']:
-        print(train_data['queryLeftNewDTO']['station_train_code'])
+# EOF
  
     
    
